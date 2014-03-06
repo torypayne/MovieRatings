4e4593276496dd3eb8503078d6543426c46d9c92
Hackbright Student <info@hackbrightacademy.com>
2014-03-06 12:56:01 -0800

    Morning of work, almost done with first part

--- model.py
+++ model.py
@@ -1,13 +1,50 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import sessionmaker
+
+ENGINE = None
+Session = None
 
 Base = declarative_base()
 
 ### Class declarations go here
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key = True)
+    email = Column(String(64), nullable=True)
+    password = Column(String(64), nullable=True)
+    age = Column(Integer, nullable=True)
+    zipcode = Column(String(15), nullable=True)
+
+class Movie(Base):
+    __tablename__ = "movies"
+
+    id = Column(Integer, primary_key = True)
+    name = Column(String(64))
+    released_at = Column(Integer, nullable=True)
+    imdb_url = Column(String(150), nullable=True)
+
+class Rating(Base):
+    __tablename__= "ratings"
+
+    id = Column(Integer, primary_key = True)
+    movie_id = Column(Integer, nullable = True)
+    user_id = Column(Integer, nullable = True)
+    rating = Column(Integer, nullable = True)
+
 
 
 ### End class declarations
+def connect():
+    global ENGINE
+    global Session
+
+    ENGINE = create_engine("sqlite:///ratings.db", echo=True)
+    Session = sessionmaker(bind=ENGINE)
+
+    return Session()
 
 def main():
     """In case we need this for something"""
--- seed.py
+++ seed.py
@@ -1,9 +1,22 @@
 import model
 import csv
 
+#open a file
+#read a line
+#parse a line
+#create an object
+#add object to session
+#commit
+#repeat
+
 def load_users(session):
-    # use u.user
-    pass
+    with open("./seed_data/u.user", "rb") as f:
+        reader = csv.reader(f, delimiter="|")
+        session = model.Session()
+        for row in reader:
+            add_user = model.User(age=row[1], zipcode=row[4])
+            session.add(add_user)
+        session.commit()
 
 def load_movies(session):
     # use u.item
@@ -15,6 +28,7 @@ def load_ratings(session):
 
 def main(session):
     # You'll call each of the load_* functions with the session as an argument
+    load_users(model.Session)
     pass
 
 if __name__ == "__main__":
