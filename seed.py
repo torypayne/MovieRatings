f8a716b5766cba397bed7e99a7f6a8a8f56ccbb3
Hackbright Student <info@hackbrightacademy.com>
2014-03-06 15:49:05 -0800

    Load Movies function working

--- seed.py
+++ seed.py
@@ -1,5 +1,6 @@
 import model
 import csv
+import datetime
 
 #open a file
 #read a line
@@ -19,8 +20,22 @@ def load_users(session):
         session.commit()
 
 def load_movies(session):
-    # use u.item
-    pass
+    with open("./seed_data/u.item", "rb") as f:
+        reader = csv.reader(f, delimiter="|")
+        session = model.Session()
+        for row in reader:
+            title =  row[1]
+            title = title.decode("latin-1")
+            if row[2] == '':
+                dt = datetime.datetime.strptime("01-Jan-1970", "%d-%b-%Y")
+            else:
+                dt = datetime.datetime.strptime(row[2], "%d-%b-%Y")
+            # print dt
+            # print "IMDB url="+row[4]
+            add_movie = model.Movie(name=title, released_at=dt, imdb_url=row[4])
+            session.add(add_movie)
+        session.commit()
+    
 
 def load_ratings(session):
     # use u.data
@@ -28,7 +43,8 @@ def load_ratings(session):
 
 def main(session):
     # You'll call each of the load_* functions with the session as an argument
-    load_users(model.Session)
+    # load_users(model.Session)
+    load_movies(model.Session)
     pass
 
 if __name__ == "__main__":
