7f49305a6b673afcdefe964e4592e6875af7163b
Hackbright Student <info@hackbrightacademy.com>
2014-03-10 12:03:07 -0700

    Working predictions for movies, now going to build them into website

--- model.py
+++ model.py
@@ -2,7 +2,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import sessionmaker, relationship, backref, scoped_session
-
+import correlation
 
 
 ENGINE = create_engine("sqlite:///ratings.db", echo=False)
@@ -22,6 +22,35 @@ class User(Base):
     password = Column(String(64), nullable=True)
     age = Column(Integer, nullable=True)
     zipcode = Column(String(15), nullable=True)
+
+    def similarity(self, other):
+        u_ratings = {}
+        paired_ratings = []
+        for r in self.ratings:
+            u_ratings[r.movie_id] = r
+
+        for r in other.ratings:
+            u_r = u_ratings.get(r.movie_id)
+            if u_r:
+                paired_ratings.append((u_r.rating, r.rating))
+
+        if paired_ratings:
+            return correlation.pearson(paired_ratings)
+        else:
+            return 0.0
+
+    def predict_rating(self, movie):
+        ratings = self.ratings
+        other_ratings = movie.ratings
+        similarities = [(self.similarity(r.user), r) \
+            for r in other_ratings ]
+        similarities.sort(reverse = True)
+        similarities = [sim for sim in similarities if sim[0] > 0]
+        if not similarities:
+            return None
+        numerator = sum([r.rating*similarity for similarity, r in similarities])
+        denominator = sum([similarity[0] for similarity in similarities])
+        return numerator/denominator
 
 class Movie(Base):
     __tablename__ = "movies"
@@ -71,7 +100,6 @@ def authenticate(email, password):
     else:
         print "Not a good password nub"
         return None
-
 
 
 # def connect():
